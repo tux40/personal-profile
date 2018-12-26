853247d1416b890ea625b4d3f7d45c53e249c6f2
aria nofriansyah <aria40nanda@gmail.com>
2018-12-26 23:57:46 +0700

    edit2 process.php

--- process.php
+++ process.php
@@ -1,24 +1,42 @@
-<?php ob_start();
+<?php 
 
 
 
-	if(isset($_POST['submit'])) {
+	// if(isset($_POST['submit'])) {
 
-	$to = "aria40nanda@gmail.com";
-	$subject = $_POST['subject'];
-	$email = $_POST['email'];
-	$txt = $_POST['message'];
-	$headers = "From: webmaster@example.com" .$email . "\r\n" .
-	"CC: aria40nanda@gmail.com";
+	// $to = "aria40nanda@gmail.com";
+	// $subject = $_POST['subject'];
+	// $email = $_POST['email'];
+	// $txt = $_POST['message'];
+	// $headers = "From: webmaster@example.com" .$email . "\r\n" .
+	// "CC: aria40nanda@gmail.com";
 
-	mail($to,$subject,$txt,$headers);
+	// mail($to,$subject,$txt,$headers);
 
-	header("Location: contact.html");
+	// header("Location: contact.html");
+	// }
 
 
-
-
+    //if "email" variable is filled out , send mail 
+    if  (isset (REQUEST['email'])){ 
+    
+	//email information 
+	$admin_email  = "aria40nanda@gmail.com";
+	$email = $_REQUEST['email'];
+	$subject = $_REQUEST['subject']; 
+	$txt = $REQUEST['message']; 
+	
+	//send mail 
+	mail($admin_email, "$subject", $txt, "From:" . $email); 
+	
+	//email response 
+	echo "thank you for contacting us ";
 	}
+	
+	
+	
+	
+	
 
 
 
